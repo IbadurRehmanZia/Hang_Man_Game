f509a3a8e5e6004a80af70be9b5fdda0f7d6978d
unknown <ibadzia3404@gmail.com>
2025-08-26 17:11:25 +0200

    lives logic applied

--- Hang_Man.py
+++ Hang_Man.py
@@ -5,30 +5,39 @@ word_list=['electricity', 'donkey', 'hardware', 'xerox', 'transistor', 'computer
 #selecting a random word
 word=random.choice(word_list)
 print(f"word={word}")
-lives=len(word)-1
+lives=6
 print(f"lives={lives}")
 
-temp_ans=""
-
+Actual_ans=[]
 
 while lives!=0:
-
-    ans=input("Enter a character that you think is in the target word:\n")
-    
-    
+    ans=input("Enter a character that you think is in the target word:")
+    temp_ans=""
     for w in word:
-        
-    
+
         if ans==w:
+            
             temp_ans+=w
-            break
+            Actual_ans.append(w)
+            
+            
+        elif w in Actual_ans:
+            
+            temp_ans +=w
+        
         else:
             temp_ans+="_"
+    if "_" not in temp_ans:
+        print("You Win!, The word was :",word)
+        break
+    if ans not in Actual_ans:
+        lives-=1
+        if lives==0:
+            print("You Lose!, The word was : ",word)
             break
-        
-    
-        
+
+            
     print(f"{temp_ans}           you have {lives} lives left")
-    
+        
             
 
